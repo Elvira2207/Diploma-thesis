f10deee1d81683252c6a0806f7d41bef1edbae12
Elvira2207 <elvirak3717@gmail.com>
2025-09-13 15:01:09 +1000

    Test_api_search

---
+++ test_api/test_api_search_movie.py
@@ -0,0 +1,79 @@
+import requests
+import pytest
+
+BASE_URL = "https://api.kinopoisk.dev/v1.4/movie"
+api_token = "4C5MWNN-QYZ4B9P-GQ6VKV9-46X6BJ0"
+
+def test_search_title_positive():
+    payload = {
+        "type":"searchRequest",
+        "id":"Лёд"
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.get(BASE_URL, json=payload, headers=headers)
+    assert resp.status_code == 200
+
+def test_search_genre_positive():
+    params = {
+        "year": 2024,
+        "genres.name": "криминал"
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.get(BASE_URL, params=params, headers=headers)
+    assert resp.status_code == 200
+
+def test_search_rating_positive():
+    params = {
+        "ratings": 8,
+        "genres.name": "спорт"
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.get(BASE_URL, params=params, headers=headers)
+    assert resp.status_code == 200
+
+def test_search_random_letters_negative():
+    payload = {
+        "type": "searchRequest",
+        "id": "Rbvbiyfz"
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.get(BASE_URL, json=payload, headers=headers)
+    assert resp.status_code == 200
+
+def test_search_specsymbol_negative():
+    params = {
+        "ratings": "!@#$%^",
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.get(BASE_URL, params=params, headers=headers)
+    assert resp.status_code == 200
+
+def test_search_other_method_negative():
+    params = {
+        "year": 2024,
+        "genres.name": "криминал"
+    }
+    headers = {
+        "X-API-KEY": api_token
+    }
+    resp = requests.post(BASE_URL, params=params, headers=headers)
+    assert resp.status_code == 404
+
+def test_search_without_token_negative():
+    params = {
+        "ratings": "9-10",
+    }
+    headers = {
+    }
+    resp = requests.get(BASE_URL, params=params, headers=headers)
+    assert resp.status_code == 401
